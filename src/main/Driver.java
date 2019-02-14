3a706909c3c730c31f980869c4edf5773c0b8c1d
BharatBaweja <bharat18csu043@ncuindia.edu>
2019-02-14 10:10:31 +0530

    Project

--- src/main/Driver.java
+++ src/main/Driver.java
@@ -1,3 +1,4 @@
+package Download_Manager.src.main;
 import src.exceptions.*;
 import src.validators.*;
 import src.processor.*;
@@ -8,10 +9,26 @@ class Driver{
 		String url;
 		String outputDirectory;
 		Scanner in = new Scanner(System.in);
+		System.out.println("---------------------------------");
+		System.out.println("\t DOWNLOAD MANAGER");
+		System.out.println("---------------------------------");
 		System.out.println("Please copy the link");
 		url = in.nextLine();
+		URLValidator linkValidator = new URLValidator();
+		boolean linkVal = linkValidator.urlValidator(url);
+		if(linkVal)
+			System.out.println("Link is valid");
+		else
+			System.out.println("Link is not valid");
 		System.out.println("Please enter the path for download");
 		outputDirectory = in.nextLine();
-		
+		CheckIfDirectoryExists pathvalidator = new CheckIfDirectoryExists();
+		boolean pathVal = pathValidator.CheckIfDirectoryExists(outputDirectory);
+		if(pathVal)
+			System.out.println("Path is valid");
+		else
+			System.out.println("Path is not valid ,please enter a valid path")
+
+
 	}
 }
