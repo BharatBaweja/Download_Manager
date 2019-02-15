de6f2a1cf11a6642d11b5f136b8524ab3cc34865
BharatBaweja <bharat18csu043@ncuindia.edu>
2019-02-15 09:58:54 +0530

    Project

--- src/com/ncu/validators/Validator.java
+++ src/com/ncu/validators/Validator.java
@@ -5,12 +5,15 @@ import java.lang.*;
 import com.ncu.exceptions.*; 
 
 class Validator{
+	String url;
+	String outputDirectory;
 		static final String URL_REGEX =
 			"^((((https?|ftps?|gopher|telnet|nntp)://)|(mailto:|news:))" +
 			"(%[0-9A-Fa-f]{2}|[-()_.!~*';/?:@&=+$,A-Za-z0-9])+)" +
 			"([).!';/?:,][[:blank:]])?$";
 	Pattern URL_PATTERN = Pattern.compile(URL_REGEX);
-	private void  URLValidator(String url) throws UrlException{
+	private void  URLValidator(String url) throws UrlException
+	{
 	Matcher matcher = URL_PATTERN.matcher(url);
 		boolean match= matcher.matches();
 		if(match==false || url==null)
@@ -20,7 +23,30 @@ class Validator{
 			 File dir = new File(outputDirectory);
              // Tests whether the directory denoted by this abstract pathname exists.
     		 boolean exists = dir.exists();
-    		 if(outputDirectory==null||exists==false)
-    		 	throw new PathException("");
+    		 if(outputDirectory.length()==0||exists==false)
+    		 	throw new PathException("error");
+		}
+	public boolean Test(){
+			//Validator v = new Validator();
+			try{
+			URLValidator(url);
+			CheckDirectory(outputDirectory);
+			}
+			catch(UrlException e){
+				e.printStackTrace();
+			}
+			catch(PathException e){
+				e.printStackTrace();
+			}
+		return false;}
+
+	}
+	class TestValidator{
+		public static void main(String args[]){
+			Validator val = new Validator();
+			String outputDirectory = "scss";
+			String url = "https://www.techiedelight.com/validate-url-java/";
+			System.out.println("SAA");
+			System.out.println(val.Test()); 
 		}
 	}
