993cbace9333d6f38518f67829d97cf0ad6f8582
BharatBaweja <bharat18csu043@ncuindia.edu>
2019-02-18 11:17:56 +0530

    project

--- src/com/ncu/main/Driver.java
+++ src/com/ncu/main/Driver.java
@@ -4,29 +4,36 @@ import com.ncu.validators.*;
 import com.ncu.processors.*;
 import java.util.*;
 import java.io.*;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
 //Main driver program
 class Driver{
-	public static void main(String args[]){
+	public static void main(String... args){
 		Validator check = new Validator();
 		String url;
 		String status;
+		String log4jConfigFile = System.getProperty("user.dir") + File.separator + "configs\\logger\\logger.properties"; 
 		String outputDirectory;
 		Scanner in = new Scanner(System.in);
 		System.out.println("---------------------------------");
 		System.out.println("\t DOWNLOAD MANAGER");
 		System.out.println("---------------------------------");
-		System.out.println("\n Please copy the link:\n");
+		Logger logger = Logger.getLogger(Driver.class);
+
+    	PropertyConfigurator.configure(log4jConfigFile);
+		logger.info("\n Please copy the link:\n");
 		url = in.nextLine();
-		System.out.println("\n Please enter outputDirectory: \n");
+		logger.info("\n Please enter outputDirectory: \n");
 		outputDirectory = in.nextLine();
 		//validate the output Directory and URL
 		check.Test(url,outputDirectory);
 		if(check.Test(url,outputDirectory)==false){
-		System.out.println("Please enter correct details");
-		System.out.println("Please copy the link");
+		logger.info("Please enter correct details");
+		logger.info("Please enter the correct link");
 		url = in.nextLine();
-		System.out.println("Please enter outputDirectory");
+		logger.info("Please enter outputDirectory");
 		outputDirectory = in.nextLine();
 			
 		}
